0f3b6973a214c4ec91fc4fd4534ff610faeb8884
Flexnetsistemas <rdassis@gmail.com>
2021-02-02 22:22:02 -0400

    Autenticação de usuário

--- routers/usuario.js
+++ routers/usuario.js
@@ -2,6 +2,23 @@ const express = require('express')
 const mysql   = require("../mysql.config").pool
 const router  = express.Router()
 
+router.post('/login', (req, res, next) => {
+  if (req.body.email === "" || req.body.senha === ""){
+      return res.status(404).send({erro: 'Informar email e Senha'})
+    }
+     mysql.getConnection((error, conn) => {
+       conn.query("SELECT email, senha from usuario WHERE (email = ?) and (senha = ?)",
+        [req.body.email, req.body.senha],
+        (error,result,fields) => {
+         conn.release()
+         if(result.length < 1){
+          return res.status(401).send({message:"Falha de Autenticação"})
+         }
+          res.status(200).send({message:"Autenticado com sucesso"})
+       })
+     })
+  })
+
 router.post('/', (req, res, next) => {
  if (req.body.email === "" || req.body.senha === ""){
         return res.status(404).send({erro: 'Informar email e Senha'})
