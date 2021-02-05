9dcd380627e14e23bc5c58ec89cc592c8a3f94af
Flexnetsistemas <rdassis@gmail.com>
2021-02-05 01:38:23 -0400

    Correção no script SQL da carga incial

--- script/Sql_Create_db.sql
+++ script/Sql_Create_db.sql
@@ -1,11 +1,17 @@
 
+DROP DATABASE IF EXISTS albumdb;
+
 CREATE DATABASE albumdb;
 
+USE albumdb;
+
+DROP TABLE IF EXISTS artista;
 CREATE TABLE artista(
 id_artista INTEGER PRIMARY KEY AUTO_INCREMENT NOT NULL,
 nome VARCHAR (100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+DROP TABLE IF EXISTS album;
 CREATE TABLE album(
 id_album INTEGER PRIMARY KEY AUTO_INCREMENT NOT NULL,
 fk_artista INTEGER NOT NULL,
@@ -14,6 +20,7 @@ capa VARCHAR(100),
  FOREIGN KEY (fk_artista) REFERENCES artista(id_artista)
 )ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+DROP TABLE IF EXISTS usuario;
 CREATE TABLE usuario(
 id_usuario INTEGER PRIMARY KEY AUTO_INCREMENT NOT NULL,
 email VARCHAR(100) NOT NULL,
@@ -25,23 +32,23 @@ INSERT INTO artista(nome) VALUES("Mike Shinoda");
 INSERT INTO artista(nome) VALUES("Michel Teló");
 INSERT INTO artista(nome) VALUES("Guns N' Roses");
 
-INSERT INTO album(fk_artista,nome,capa) VALUES(1,"Harakiri",null);
-INSERT INTO album(fk_artista,nome,capa) VALUES(1,"Black Blooms",null);
-INSERT INTO album(fk_artista,nome,capa) VALUES(1,"The Rough Dog",null);
+INSERT INTO album(fk_artista,nomeAlbum,capa) VALUES(1,"Harakiri","");
+INSERT INTO album(fk_artista,nomeAlbum,capa) VALUES(1,"Black Blooms","");
+INSERT INTO album(fk_artista,nomeAlbum,capa) VALUES(1,"The Rough Dog","");
 
-INSERT INTO album(fk_artista,nome,capa) VALUES(2,"The Rising Tied",null);
-INSERT INTO album(fk_artista,nome,capa) VALUES(2,"Post Traumatic",null);
-INSERT INTO album(fk_artista,nome,capa) VALUES(2,"Post Traumatic EP",null);
-INSERT INTO album(fk_artista,nome,capa) VALUES(2,"Where'd You Go",null);
+INSERT INTO album(fk_artista,nomeAlbum,capa) VALUES(2,"The Rising Tied","");
+INSERT INTO album(fk_artista,nomeAlbum,capa) VALUES(2,"Post Traumatic","");
+INSERT INTO album(fk_artista,nomeAlbum,capa) VALUES(2,"Post Traumatic EP","");
+INSERT INTO album(fk_artista,nomeAlbum,capa) VALUES(2,"Where'd You Go","");
 
-INSERT INTO album(fk_artista,nome,capa) VALUES(3,"Bem Sertanejo",null);
-INSERT INTO album(fk_artista,nome,capa) VALUES(3,"Bem Sertanejo - O Show (AoVivo)",null);
-INSERT INTO album(fk_artista,nome,capa) VALUES(3,"Bem Sertanejo - (1ª Temporada) - EP",null);
-INSERT INTO album(fk_artista,nome,capa) VALUES(3,"Where'd You Go",null);
+INSERT INTO album(fk_artista,nomeAlbum,capa) VALUES(3,"Bem Sertanejo","");
+INSERT INTO album(fk_artista,nomeAlbum,capa) VALUES(3,"Bem Sertanejo - O Show (AoVivo)","");
+INSERT INTO album(fk_artista,nomeAlbum,capa) VALUES(3,"Bem Sertanejo - (1ª Temporada) - EP","");
+INSERT INTO album(fk_artista,nomeAlbum,capa) VALUES(3,"Where'd You Go","");
 
-INSERT INTO album(fk_artista,nome,capa) VALUES(4,"Use Your IIIlusion I",null);
-INSERT INTO album(fk_artista,nome,capa) VALUES(4,"Use Your IIIlusion II",null);
-INSERT INTO album(fk_artista,nome,capa) VALUES(4,"Greatest Hits",null);
+INSERT INTO album(fk_artista,nomeAlbum,capa) VALUES(4,"Use Your IIIlusion I","");
+INSERT INTO album(fk_artista,nomeAlbum,capa) VALUES(4,"Use Your IIIlusion II","");
+INSERT INTO album(fk_artista,nomeAlbum,capa) VALUES(4,"Greatest Hits","");
 
 
 
